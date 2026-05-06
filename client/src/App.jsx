c8852056f4e20dc78609f56acd35c4f06dc5a693
vjp-ikerCQ <iclementeq01@informatica.iesvalledeljerteplasencia.es>
2026-05-06 18:46:23 +0200

    chore: initialize node_modules directory with project dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeScreen from './HomeScreen'
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <HomeScreen />
+    </div>
+  )
+}
+
+export default App
