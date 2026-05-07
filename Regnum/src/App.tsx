3ad9ef4b1681fd2c49c702fb2040b4cc97d98500
vjp-pabloSM <pserranom08@informatica.iesvalledeljerteplasencia.es>
2026-05-07 17:37:23 +0200

    Creación del proyecto y carpetas

---
+++ Regnum/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Regnum Hollow</h1>
+      <h2>Voluntas Vincit</h2>
+    </div>
+  )
+}
+
+export default App
