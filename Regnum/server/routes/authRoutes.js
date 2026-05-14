3793124e69d5e7d4e3345d9e79788490e37e2dbb
Antonio <aperezc88@educarex.es>
2026-05-14 10:36:54 +0200

    añadida logica para eliminar usuario solo en el backend

--- Regnum/server/routes/authRoutes.js
+++ Regnum/server/routes/authRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { login, register } = require('../controllers/authController');
+const { login, register, deleteAccount } = require('../controllers/authController');
 
 router.post('/login', login);
 router.post('/register', register);
+router.delete('/delete-account', deleteAccount);
 
 module.exports = router;
